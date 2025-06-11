436fd045ab86fbac9ac411485a05058507728ff8
Tihetna1891 <bettygeb1990@gmail.com>
2025-06-11 20:56:03 +0300

    add logging to init_db

--- app/init_db.py
+++ app/init_db.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from .models import Base, User
 from .auth import get_password_hash
 import os
-
+import logging
+logger = logging.getLogger(__name__)
+logger.info("Initializing database and creating admin user")
 database_url = os.getenv("DATABASE_URL")
 if not database_url:
     raise ValueError("DATABASE_URL environment variable is not set")
@@ -15,7 +17,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 db = SessionLocal()
 
 if not db.query(User).filter(User.username == "admin").first():
+    logger.info("Admin user created successfully")
     db.add(User(username="admin", password=get_password_hash("adminpass"), role="ADMIN"))
     db.commit()
+else:
+    logger.info("Admin user already exists")
 
 db.close()
